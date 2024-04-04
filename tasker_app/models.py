569e487c54f15012e6ca58a1a213cc69f6ac4d52
maria_sz <kkeraz@gmail.com>
2024-04-04 23:27:23 +0200

    add user +admin

--- tasker_app/models.py
+++ tasker_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from .menagers import UserManager
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
