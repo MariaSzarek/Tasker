45303c72d1a912d8deda3a643ee62f3b3bb8c505
maria_sz <kkeraz@gmail.com>
2024-04-10 23:22:56 +0200

    user mail verification in model

--- tasker_app/models.py
+++ tasker_app/models.py
@@ -3,10 +3,9 @@ from .menagers import UserManager
 from django.contrib.auth.models import AbstractUser
 
 
-
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     username = None
 
